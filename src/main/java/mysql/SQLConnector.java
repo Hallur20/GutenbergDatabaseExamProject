31b294b9690fefee31fce6cc56e4e847a077ad30
Mokayed <marik.3000@hotmail.com>
2019-05-30 07:12:56 +0200

    Update SQLConnector.java

--- src/main/java/mysql/SQLConnector.java
+++ src/main/java/mysql/SQLConnector.java
@@ -7,6 +7,6 @@ import java.sql.SQLException;
 public class SQLConnector {
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/Gutenberg?serverTimezone=UTC", "user", "1234");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/gutenberg?serverTimezone=UTC", "user", "1234");
     }
 }
